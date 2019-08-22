581aef77a9ad169b91b9369e3babd82f7675e5e1
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-08-22 13:46:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,7 +33,7 @@ class Song
       genre_count = {}
       @@genres.each do |genre|
         if genre_count[genre]
-          genre_count[genre += 1]
+          genre_count[genre] += 1
         else
           genre_count[genre] = 1
         end
