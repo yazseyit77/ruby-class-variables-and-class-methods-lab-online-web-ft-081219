f5223327060628fd34c9da137cf2dc5126055316
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-08-22 13:38:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,10 +2,18 @@ class Song
 
 attr_accessor :name, :genre, :artist
 
+@@count = 0
+
+@@artists = []
+@@genres = []
+
 def initialize(name, artist, genre)
   @name = name
   @artist = artist
   @genre = genre
+  @@genres << genre
+  @@artists << artist
+  @@count += 1
 end
 
 end
